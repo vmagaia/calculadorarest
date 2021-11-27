2e399537d2b421aebc1094141977e63669b7c114
Vicente Magaia <vcmagaia@gmail.com>
2021-11-27 21:14:31 +0200

    Initial Commit

---
+++ src/main/java/mz/co/calculadorarest/CalculadorarestApplication.java
@@ -0,0 +1,13 @@
+package mz.co.calculadorarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadorarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadorarestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mz/co/calculadorarest/CalculadorarestApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.calculadorarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadorarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
